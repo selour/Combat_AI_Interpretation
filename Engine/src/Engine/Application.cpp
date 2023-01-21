e9d34524b1fb4a172d9645589f03d9d32a27d4e9
selour <selour13@gmail.com>
2023-01-21 19:12:01 +0800

    updata

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -9,6 +9,7 @@ namespace Engine
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
+
 	}
 	Application::~Application()
 	{
